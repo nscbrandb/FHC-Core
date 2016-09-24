243e6f4ab26fc4ae3815692b5f1000f2aa83af4d
nscbrandb <bernhard.brandstetter@fhstp.ac.at>
2016-09-24 21:17:59 +0200

    Verhübschung für Lektoren-Tree und Menulist

--- rdf/mitarbeiter.rdf.php
+++ rdf/mitarbeiter.rdf.php
@@ -19,159 +19,124 @@
  *          Andreas Oesterreicher <andreas.oesterreicher@technikum-wien.at> and
  *          Rudolf Hangl <rudolf.hangl@technikum-wien.at>.
  */
-
-require_once('../config/vilesci.config.inc.php');
-require_once('../include/person.class.php');
-require_once('../include/benutzer.class.php');
-require_once('../include/mitarbeiter.class.php');
-require_once('../include/benutzerberechtigung.class.php');
-require_once('../include/studiengang.class.php');
-require_once('../include/functions.inc.php');
-// header for no cache
-header("Cache-Control: no-cache");
-header("Cache-Control: post-check=0, pre-check=0",false);
-header("Expires Mon, 26 Jul 1997 05:00:00 GMT");
-header("Pragma: no-cache");
-// content type setzen
-header("Content-type: application/xhtml+xml");
-
-if (isset($_GET['lektor']))
-{
-	if($_GET['lektor']=='true')
-		$lektor=true;
-	else 
-		$lektor=false;
-}
-else
-	$lektor=null;
-
-if (isset($_GET['fixangestellt']))
-	$fixangestellt=$_GET['fixangestellt'];
-else
-	$fixangestellt=null;
-
-if (isset($_GET['stg_kz']))
-	$stg_kz=$_GET['stg_kz'];
-else
-	$stg_kz=null;
-
-if (isset($_GET['fachbereich_id']))
-	$fachbereich_id=$_GET['fachbereich_id'];
-else
-	$fachbereich_id=null;
-
-if (isset($_GET['user']))
-	$user=$_GET['user'];
-else
-	$user=false;
+	require_once('../config/vilesci.config.inc.php');
+	require_once('../include/person.class.php');
+	require_once('../include/benutzer.class.php');
+	require_once('../include/mitarbeiter.class.php');
+	require_once('../include/benutzerberechtigung.class.php');
+	require_once('../include/studiengang.class.php');
+	require_once('../include/functions.inc.php');
+	// header for no cache
+	header("Cache-Control: no-cache");
+	header("Cache-Control: post-check=0, pre-check=0",false);
+	header("Expires Mon, 26 Jul 1997 05:00:00 GMT");
+	header("Pragma: no-cache");
+	// content type setzen
+	header("Content-type: application/xhtml+xml");
 	
-if(isset($_GET['filter']))
-	$filter = $_GET['filter'];
-else 
-	$filter=null;
+	$lektor = optional_param('lektor',null,PARAM_CONVBOOL);
+	$fixangestellt = optional_param('fixangestellt',null,PARAM_CONVBOOL);
+	$stg_kz = optional_param('stg_kz',null,PARAM_INT);
+	$fachbereich_id = optional_param('fachbereich_id',null,PARAM_INT);
+	$user = optional_param('user',false,PARAM_CONVBOOL);
+	$filter = optional_param('filter',null,PARAM_NOTAGS);
+	$mitarbeiter_uid = optional_param('mitarbeiter_uid',null,PARAM_NOTAGS);	
+	$aktiv = optional_param('aktiv',false,PARAM_CONVBOOL);
+	$lehrveranstaltung_id = optional_param('lehrveranstaltung_id',null,PARAM_INT);
+	$filter2 = optional_param('filter2',null,PARAM_NOTAGS);
 	
-if(isset($_GET['mitarbeiter_uid']))
-	$mitarbeiter_uid=$_GET['mitarbeiter_uid'];
-else 
-	$mitarbeiter_uid=null;
-
-if(isset($_GET['lehrveranstaltung_id']) && is_numeric($_GET['lehrveranstaltung_id']))
-{
-	$lehrveranstaltung_id = $_GET['lehrveranstaltung_id'];
-	echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
-	$mitarbeiter=new mitarbeiter();
-}
-else 	
-{
-	echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
-	$lehrveranstaltung_id=null;
-	$mitarbeiter=new mitarbeiter();
-}
-
-// Mitarbeiter holen
-
-$rdf_url='http://www.technikum-wien.at/mitarbeiter/';
-
-echo '
-<RDF:RDF
-	xmlns:RDF="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-	xmlns:MITARBEITER="'.$rdf_url.'rdf#"
->
-';
-
-function draw_row($mitarbeiter)
-{
-	global $rdf_url;
+	$rdf_url='http://www.technikum-wien.at/mitarbeiter/';
+	$mitarbeiter = new mitarbeiter();
 	
-	echo '
-	<RDF:Description about="'.$rdf_url.$mitarbeiter->uid.'" >
-    	<MITARBEITER:uid><![CDATA['.$mitarbeiter->uid.']]></MITARBEITER:uid>
-		<MITARBEITER:titelpre><![CDATA['.$mitarbeiter->titelpre.']]></MITARBEITER:titelpre>
-		<MITARBEITER:titelpost><![CDATA['.$mitarbeiter->titelpost.']]></MITARBEITER:titelpost>
-		<MITARBEITER:vornamen><![CDATA['.$mitarbeiter->vornamen.']]></MITARBEITER:vornamen>
-		<MITARBEITER:vorname><![CDATA['.$mitarbeiter->vorname.']]></MITARBEITER:vorname>
-		<MITARBEITER:nachname><![CDATA['.$mitarbeiter->nachname.']]></MITARBEITER:nachname>
-		<MITARBEITER:kurzbz><![CDATA['.$mitarbeiter->kurzbz.']]></MITARBEITER:kurzbz>
-		<MITARBEITER:studiengang_kz></MITARBEITER:studiengang_kz>
-  	</RDF:Description>
-  	';
-}
-
-if($lehrveranstaltung_id==null && $filter==null && $mitarbeiter_uid==null)
-{
-	$ma=$mitarbeiter->getMitarbeiter($lektor,$fixangestellt,$stg_kz);
+	echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';	
+	echo '<RDF:RDF
+				xmlns:RDF="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+				xmlns:MITARBEITER="'.$rdf_url.'rdf#"
+		>
+		';
 	
-	$stg_obj = new studiengang();
-	$stg_obj->getAll('typ, kurzbz', false);
-	foreach ($stg_obj->result as $stg)
-		$stg_arr[$stg->studiengang_kz]=$stg->kuerzel;
+	//FUNKTION
 	
-	$alle='';
-	foreach ($ma as $mitarbeiter)
-	{
-		draw_row($mitarbeiter);
-		$alle.="\n\t\t\t<RDF:li resource=\"".$rdf_url.$mitarbeiter->uid."\" />";
+	function checkfilter($row, $filter2) {
+		global $studiensemester_kurzbz, $kontofilterstg;
+		$db = new basis_db();
+	
+		if($filter2=='konto') {
+			// Alle Personen die offene Buchungen haben
+			$qry = "SELECT sum(betrag) as summe FROM tbl_konto WHERE person_id='$row->person_id'";
+			print($qry);
+			if($db->db_query($qry))
+				if($row_filter = $db->db_fetch_object())
+					if($row_filter->summe=='0.00' || $row_filter->summe=='' || $row_filter->summe=='0')
+						return false;
+		} elseif(strstr($filter2,'buchungstyp;')) {
+			// Alle Personen die keine Belastung auf den uebergebenen Buchungstyp haben 
+			// Incoming werden nicht beruecksichtigt
+			list($filter, $buchungstyp) = split(';',$filter2);
+			
+			$qry = "SELECT count(*) as anzahl FROM public.tbl_konto WHERE 
+						studiensemester_kurzbz='".addslashes($studiensemester_kurzbz)."' AND 
+						person_id='".addslashes($row->person_id)."' AND 
+						buchungstyp_kurzbz='$buchungstyp'";
+			if($db->db_query($qry)) {
+				if($row_filter = $db->db_fetch_object()) {
+					if($row_filter->anzahl>0 ) return false;
+				}
+			}
+		}
+		return true;
 	}
-	$desc= '
-			<RDF:Description about="'.$rdf_url.'_alle" >
-				<MITARBEITER:uid></MITARBEITER:uid>
-				<MITARBEITER:titelpre></MITARBEITER:titelpre>
-				<MITARBEITER:titelpost></MITARBEITER:titelpost>
-				<MITARBEITER:vornamen></MITARBEITER:vornamen>
-				<MITARBEITER:vorname></MITARBEITER:vorname>
-				<MITARBEITER:nachname></MITARBEITER:nachname>
-				<MITARBEITER:kurzbz>Alle</MITARBEITER:kurzbz>
-				<MITARBEITER:studiengang_kz></MITARBEITER:studiengang_kz>
-			</RDF:Description>
-	';
-	
-	$seq= "
-	<RDF:Seq about=\"".$rdf_url."liste\" >
-		<RDF:li>
-			<RDF:Seq about=\"".$rdf_url."_alle\" >$alle
-			</RDF:Seq>
-		</RDF:li>
-		";
-	
-	if ($user)
-	{
-		$bb=new benutzerberechtigung();
-		if($bb->getBerechtigungen(get_uid()))
-		{
-			$stge=$bb->getStgKz('admin');
-			$stge=array_merge($stge, $bb->getStgKz('assistenz'));
-			$ma=$mitarbeiter->getMitarbeiterStg($lektor,$fixangestellt,$stge, 'lkt');
-			$laststg=-1;
-			if(count($ma)>0)
-			{
-				foreach ($ma as $mitarbeiter)
-				{
-					if($mitarbeiter->studiengang_kz!=$laststg)
-					{
-						if($laststg!=-1)
-						{
-							$seq.="\n\t\t</RDF:Seq>\n\t</RDF:li>\n";
-						}
+		
+	if($lehrveranstaltung_id == null && $filter == null && $mitarbeiter_uid == null) {
+		$ma = $mitarbeiter->getMitarbeiter($lektor,$fixangestellt,$stg_kz);
+
+		$stg_obj = new studiengang();
+		$stg_obj->getAll('typ, kurzbz', false);
+		foreach ($stg_obj->result as $stg) $stg_arr[$stg->studiengang_kz] = $stg->kuerzel;
+		
+		$alle = '';
+		foreach ($ma as $mitarbeiter) {
+			if ($aktiv && $mitarbeiter->aktiv != true) continue;
+			else {
+				draw_row($mitarbeiter);
+				$alle .= "\n\t\t\t<RDF:li resource=\"".$rdf_url.$mitarbeiter->uid."\" />";
+			}
+		}
+		$desc= '
+				<RDF:Description about="'.$rdf_url.'_alle" >
+					<MITARBEITER:uid></MITARBEITER:uid>
+					<MITARBEITER:titelpre></MITARBEITER:titelpre>
+					<MITARBEITER:titelpost></MITARBEITER:titelpost>
+					<MITARBEITER:vornamen></MITARBEITER:vornamen>
+					<MITARBEITER:vorname></MITARBEITER:vorname>
+					<MITARBEITER:nachname></MITARBEITER:nachname>
+					<MITARBEITER:kurzbz>Alle</MITARBEITER:kurzbz>
+					<MITARBEITER:aktiv></MITARBEITER:aktiv>
+					<MITARBEITER:aktivbild></MITARBEITER:aktivbild>
+					<MITARBEITER:studiengang_kz></MITARBEITER:studiengang_kz>
+				</RDF:Description>
+		';
+		
+		$seq= '
+			<RDF:Seq about="'.$rdf_url.'liste" >
+				<RDF:li>
+					<RDF:Seq about="'.$rdf_url.'_alle" >'.$alle.'
+					</RDF:Seq>
+				</RDF:li>
+			';
+		
+		if ($user) {
+			$bb = new benutzerberechtigung();
+			if($bb->getBerechtigungen(get_uid())) {
+				$stge = $bb->getStgKz('admin');
+				$stge = array_merge($stge, $bb->getStgKz('assistenz'));
+				$ma = $mitarbeiter->getMitarbeiterStg($lektor,$fixangestellt,$stge, 'lkt',true);
+				$laststg = -1;
+				foreach ($ma as $mitarbeiter) {
+					if ($aktiv && !$mitarbeiter->aktiv) continue;
+					if ($mitarbeiter->studiengang_kz < 100) continue;
+					if($mitarbeiter->studiengang_kz != $laststg) {
+						if($laststg!=-1) $seq.="\n\t\t</RDF:Seq>\n\t</RDF:li>\n";
 						$desc.="\n\t\t<RDF:Description about=\"".$rdf_url.$mitarbeiter->studiengang_kz."\" >".
 								"\n\t\t\t<MITARBEITER:uid></MITARBEITER:uid>".
 								"\n\t\t\t<MITARBEITER:titelpre></MITARBEITER:titelpre>".
@@ -180,78 +145,84 @@ if($lehrveranstaltung_id==null && $filter==null && $mitarbeiter_uid==null)
 								"\n\t\t\t<MITARBEITER:vorname></MITARBEITER:vorname>".
 								"\n\t\t\t<MITARBEITER:nachname></MITARBEITER:nachname>".
 								"\n\t\t\t<MITARBEITER:kurzbz>".$stg_arr[$mitarbeiter->studiengang_kz]."</MITARBEITER:kurzbz>".
+								"\n\t\t\t<MITARBEITER:aktiv></MITARBEITER:aktiv>".
+								"\n\t\t\t<MITARBEITER:aktivbild></MITARBEITER:aktivbild>".
 								"\n\t\t\t<MITARBEITER:studiengang_kz>$mitarbeiter->studiengang_kz</MITARBEITER:studiengang_kz>".
 								"\n\t\t</RDF:Description>\n";
-	
-						$seq.="\n\t<RDF:li>\n\t\t<RDF:Seq about=\"".$rdf_url.$mitarbeiter->studiengang_kz."\" >";
-	
+		
+						$seq .= "\n\t<RDF:li>\n\t\t<RDF:Seq about=\"".$rdf_url.$mitarbeiter->studiengang_kz."\" >";
 						$laststg = $mitarbeiter->studiengang_kz;
 					}
-					$seq.="\n\t\t\t<RDF:li resource=\"".$rdf_url.$mitarbeiter->uid."\" />";
+					$seq .= "\n\t\t\t<RDF:li resource=\"".$rdf_url.$mitarbeiter->uid."\" />";
 				}
-				$seq.="\n\t\t</RDF:Seq>\n\t</RDF:li>";
+				$seq .= "\n\t\t</RDF:Seq>\n\t</RDF:li>";
+			}
+		}
+		echo $desc;
+		echo $seq;
+	} else {
+		//$filter = utf8_encode($filter);
+		echo "<RDF:Seq about=\"".$rdf_url."liste\" >";
+		if(isset($_GET['optional']) && $_GET['optional'] == 'true') {
+			echo '
+				<RDF:li>
+				<RDF:Description about="" >
+			    	<MITARBEITER:uid><![CDATA[]]></MITARBEITER:uid>
+					<MITARBEITER:titelpre><![CDATA[]]></MITARBEITER:titelpre>
+					<MITARBEITER:titelpost><![CDATA[]]></MITARBEITER:titelpost>
+					<MITARBEITER:vornamen><![CDATA[]]></MITARBEITER:vornamen>
+					<MITARBEITER:vorname><![CDATA[]]></MITARBEITER:vorname>
+					<MITARBEITER:nachname><![CDATA[-- Keine Auswahl --]]></MITARBEITER:nachname>
+					<MITARBEITER:kurzbz><![CDATA[]]></MITARBEITER:kurzbz>
+					<MITARBEITER:aktiv><![CDATA[]]></MITARBEITER:aktiv>
+					<MITARBEITER:aktivbild><![CDATA[]]></MITARBEITER:aktivbild>
+					<MITARBEITER:studiengang_kz></MITARBEITER:studiengang_kz>
+			  	</RDF:Description>
+			  	</RDF:li>
+			  	';
+		}
+		
+		if($mitarbeiter_uid != null) {
+			$mitarbeiter->load($mitarbeiter_uid);
+			echo " 
+				<RDF:li>
+					<RDF:Seq about=\"".$rdf_url."_alle\" >
+		      			<RDF:li>";
+			draw_row($mitarbeiter);
+			echo "
+						</RDF:li>
+					</RDF:Seq>
+				</RDF:li>";
+		} else {
+			if($filter == null) $mitarbeiter->getMitarbeiterFromLehrveranstaltung($lehrveranstaltung_id);
+			else $mitarbeiter->getMitarbeiterFilter($filter);
+			foreach ($mitarbeiter->result as $row) {
+				if(checkfilter($row, $filter2)) {
+					echo '<RDF:li>';
+					draw_row($row);
+					echo '</RDF:li>';
+				}
 			}
 		}
 	}
-	echo $desc;
-	echo $seq;
-}
-else 
-{
-	$filter = $filter;
-	echo "<RDF:Seq about=\"".$rdf_url."liste\" >";
-	if(isset($_GET['optional']) && $_GET['optional']=='true')
-	{
+	echo '</RDF:Seq>';
+	echo '</RDF:RDF>';
+	
+	function draw_row($mitarbeiter)	{
+		global $rdf_url;
 		echo '
-		<RDF:li>
-		<RDF:Description about="" >
-	    	<MITARBEITER:uid><![CDATA[]]></MITARBEITER:uid>
-			<MITARBEITER:titelpre><![CDATA[]]></MITARBEITER:titelpre>
-			<MITARBEITER:titelpost><![CDATA[]]></MITARBEITER:titelpost>
-			<MITARBEITER:vornamen><![CDATA[]]></MITARBEITER:vornamen>
-			<MITARBEITER:vorname><![CDATA[]]></MITARBEITER:vorname>
-			<MITARBEITER:nachname><![CDATA[-- Keine Auswahl --]]></MITARBEITER:nachname>
-			<MITARBEITER:kurzbz><![CDATA[]]></MITARBEITER:kurzbz>
+		<RDF:Description about="'.$rdf_url.$mitarbeiter->uid.'" >
+	    	<MITARBEITER:uid><![CDATA['.$mitarbeiter->uid.']]></MITARBEITER:uid>
+			<MITARBEITER:titelpre><![CDATA['.$mitarbeiter->titelpre.']]></MITARBEITER:titelpre>
+			<MITARBEITER:titelpost><![CDATA['.$mitarbeiter->titelpost.']]></MITARBEITER:titelpost>
+			<MITARBEITER:vornamen><![CDATA['.$mitarbeiter->vornamen.']]></MITARBEITER:vornamen>
+			<MITARBEITER:vorname><![CDATA['.$mitarbeiter->vorname.']]></MITARBEITER:vorname>
+			<MITARBEITER:nachname><![CDATA['.$mitarbeiter->nachname.']]></MITARBEITER:nachname>
+			<MITARBEITER:kurzbz><![CDATA['.$mitarbeiter->kurzbz.']]></MITARBEITER:kurzbz>
+			<MITARBEITER:aktiv><![CDATA['.($mitarbeiter->aktiv?'true':'false').']]></MITARBEITER:aktiv>
+			<MITARBEITER:aktivbild><![CDATA['.($mitarbeiter->aktiv?'../../addons/STPCore/skin/images/onbe_ok.png':'../../addons/STPCore/skin/images/onbe_open.png').']]></MITARBEITER:aktivbild>
 			<MITARBEITER:studiengang_kz></MITARBEITER:studiengang_kz>
 	  	</RDF:Description>
-	  	</RDF:li>
 	  	';
 	}
-	
-	if($mitarbeiter_uid!=null)
-	{
-		$mitarbeiter->load($mitarbeiter_uid);
-		echo " 
-		<RDF:li>
-			<RDF:Seq about=\"".$rdf_url."_alle\" >
-      			<RDF:li>";
-		draw_row($mitarbeiter);
-		echo "
-				</RDF:li>
-			</RDF:Seq>
-		</RDF:li>";
-	}
-	else 
-	{
-		if($filter==null)
-		{
-			$mitarbeiter->getMitarbeiterFromLehrveranstaltung($lehrveranstaltung_id);
-		}
-		else 
-		{
-			$mitarbeiter->getMitarbeiterFilter($filter);
-		}
-		
-		foreach ($mitarbeiter->result as $row)
-		{
-			echo '<RDF:li>';
-			draw_row($row);
-			echo '</RDF:li>';
-		}
-	}
-}
-
 ?>
-
-</RDF:Seq>
-</RDF:RDF>
