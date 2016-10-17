bf01c9fa9d4f52ac16cd0cc05da7876f5645571a
nscbrandb <bernhard.brandstetter@fhstp.ac.at>
2016-10-17 14:01:42 +0200

    Load Addons init.js

--- content/tempus.xul.php
+++ content/tempus.xul.php
@@ -20,6 +20,7 @@ require_once('../config/vilesci.config.inc.php');
 require_once('../include/functions.inc.php');
 require_once('../include/benutzerberechtigung.class.php');
 require_once('../include/studiensemester.class.php');
+require_once('../include/addon.class.php');
 
 $uid=get_uid();
 $error_msg='';
